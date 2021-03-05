02759c5f591997613a119c3de8942e2f9a596d7b
Preeti8600 <33160356+Preeti8600@users.noreply.github.com>
2021-03-05 14:22:03 +0530

    Update FileUtils.java

--- spring-cloud-alibaba-starters/spring-cloud-alibaba-commons/src/main/java/com/alibaba/cloud/commons/io/FileUtils.java
+++ spring-cloud-alibaba-starters/spring-cloud-alibaba-commons/src/main/java/com/alibaba/cloud/commons/io/FileUtils.java
@@ -109,5 +109,25 @@ public final class FileUtils {
 	public static String readFileToString(final File file) throws IOException {
 		return readFileToString(file, Charset.defaultCharset());
 	}
+	
+	 public void process() {
+        SecureRandom secureRandom = new SecureRandom();
+	     System.runFinalizersOnExit(true);
+        try {
+            PrintWriter out = new PrintWriter("");
+            out.println("the text");
+System.out.println("");
+System.out.println("");   
+            out.close();
+            Cipher c1 = Cipher.getInstance("DES");
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+            System.err.println();
+System.err.println();
+
+       
+
+        }
+
+    }
 
 }
